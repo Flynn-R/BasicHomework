f8454c671886d8d248a0bf6c49891d24e56a7946
Flynn <seyfullin.contact@icloud.com>
2022-01-01 22:12:48 +0300

    Homework 6

---
+++ Домашнее задание № 6/Homework 6.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+// Task 1
+int* arrayInit1();
+void arrayFill(int*);
+void arrayOutput(const int*);
+
+// Task 2
+int** arrayInit2();
+void arrayFill(int**);
+void arrayOutput(int**);
+void arrayClear(int**);
+
+// Task 3
+void createFile();
+
+// Task 4
+void cat();
+
+// Task 5
+void find();
+
+int main()
+{
+    // Задание 1
+    int* ptr = arrayInit1();
+    if (ptr != nullptr)
+    {
+        arrayFill(ptr);
+        arrayOutput(ptr);
+        delete[] ptr;
+        ptr = nullptr;
+    }
+    else
+    {
+        std::cerr << "Error\n\n";
+    }
+
+    // Задание 2
+    int** pPtr = arrayInit2();
+    if (pPtr != nullptr)
+    {
+        arrayFill(pPtr);
+        arrayOutput(pPtr);
+        arrayClear(pPtr);
+    }
+    else
+    {
+        std::cerr << "Error\n\n";
+    }
+
+    // Задание 3
+    createFile();
+
+    // Задание 4
+    cat();
+
+    // Задание 5
+    find();
+}
---
+++ Домашнее задание № 6/Task 3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+void createFile()
+{
+	std::cout << "Enter the first file name: ";
+	std::string path1;
+	std::cin >> path1;
+	std::ofstream file1(path1 + ".txt");
+	std::cout << "Enter the second file name: ";
+	std::string path2;
+	std::cin >> path2;
+	std::cout << "\n";
+	std::ofstream file2(path2 + ".txt");
+
+	if (file1.is_open() && file2.is_open())
+	{
+		unsigned short N1 = (rand() % 51) + 51;
+		unsigned short N2 = (rand() % 51) + 51;
+		char* buf1 = new (std::nothrow) char[N1];
+		char* buf2 = new (std::nothrow) char[N2];
+		if (buf1 != nullptr && buf2 != nullptr)
+		{
+			for (size_t i = 0; i < N1; i++)
+			{
+				if (i == N1 - 1)
+				{
+					buf1[i] = '\0';
+				}
+				else
+				{
+					buf1[i] = (rand() % 95) + 32;
+				}
+			}
+			for (size_t i = 0; i < N2; i++)
+			{
+				if (i == N2 - 1)
+				{
+					buf2[i] = '\0';
+				}
+				else
+				{
+					buf2[i] = (rand() % 95) + 32;
+				}
+			}
+
+			file1 << buf1;
+			file1.close();
+			file2 << buf2;
+			file2.close();
+		}
+		else
+		{
+			std::cerr << "Error\n\n";
+		}
+	}
+	else
+	{
+		std::cerr << "Error\n";
+	}
+}
